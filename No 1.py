8602f0c30bcfee4693cb5bf7fa15104ccd0a45d4
AkhmadRezkyUtama261 <ahkmadrezkyutama369@gmail.com>
2025-10-06 10:50:03 +0700

    no1

---
+++ No 1.py
@@ -0,0 +1,18 @@
+nilai = int(input("Masukkan Nilai Anda: "))
+
+if nilai >= 90:
+    print ("PERFECTTT")
+
+elif nilai >= 80:
+    print ("Gokill abiezzzz")
+
+elif nilai >= 70:
+    print ("Niceee")
+
+elif nilai >= 60:
+    print ("Belajar Lagi ya dek")
+
+else:
+    print ("Wallahu a'lam bish shawab")
+
+    
